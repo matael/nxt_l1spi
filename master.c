6c7dfd3b035d80d51d73bb2df0286a3c47b42d28
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2012-01-07 12:43:20 +0100

    Ajout des templates de programme master et slave.
    Contiennent :
    	-> header
    	-> inclusion de protocol.h
    	-> tache principale vide

---
+++ master.c
@@ -0,0 +1,17 @@
+/*
+ * NXT to NXT bt communication
+ * Master brick.
+ *
+ * Authors :
+ *  Lucas Bourneuf
+ *  Mathieu Gaborit
+ * 
+ * Date    : Janvier 2012
+ * License : WTFPL
+ *
+ */
+
+#include "protocol.h"
+
+task main(){
+}
---
+++ slave.c
@@ -0,0 +1,17 @@
+/*
+ * NXT to NXT bt communication
+ * Slave brick.
+ *
+ * Authors :
+ *  Lucas Bourneuf
+ *  Mathieu Gaborit
+ * 
+ * Date    : Janvier 2012
+ * License : WTFPL
+ *
+ */
+
+#include "protocol.h"
+
+task main(){
+}
