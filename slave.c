e5e12f39137c0a51ec75acddaffe79a27eb2bf22
Aluriak <lucas.bourneuf@laposte.net>
2012-01-08 18:14:15 +0100

    Première version du code de slave

--- slave.c
+++ slave.c
@@ -14,4 +14,22 @@
 #include "protocol.h"
 
 task main(){
-}
+  // wait for a connection to the master
+  BT_WaitConn(MASTER);
+  
+  
+  // connection etablished
+  while(true){
+    // wait & read the master's order
+    byte *order = BT_ReadFromMaster();
+    
+    // call the right function
+    if (order[0] == BOT_ROTATE_MOTOR_EX) {
+        BT_RotateMotorEx(order[1], order[2], order[3], order[4], order[5]);
+    } else if (order[0] == BOT_ON_FWD) {
+        BT_OnFwd(order[1], order[2]);
+    } else {
+        __BT_OneByteFunc(order[1]);
+    }
+  } // end while
+} // end task main()
